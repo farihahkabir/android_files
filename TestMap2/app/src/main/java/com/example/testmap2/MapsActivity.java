66373763c261fce6844fa85e6f484e66176fbb2f
Farihah Kabir <nadahkabir@gmail.com>
2019-07-27 19:21:23 +0600

    test map 2: different types of maps

--- TestMap2/app/src/main/java/com/example/testmap2/MapsActivity.java
+++ TestMap2/app/src/main/java/com/example/testmap2/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng gazipur = new LatLng(23.968841,  90.386535);
+        mMap.addMarker(new MarkerOptions().position(gazipur).title("Marker in Gazipur"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(gazipur, 10F));
     }
 }
